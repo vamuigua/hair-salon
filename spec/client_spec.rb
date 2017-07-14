3eae57d284ff8902798be3fab0edc72a22008ff6
Victor Allen <vamuigua@gmail.com>
2017-07-14 16:37:11 +0300

    Added spec to save a client

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -10,4 +10,13 @@ describe(Client) do
         expect(Client.all()).to(eq([]))
     	end
 	end
+
+	#spec to save a client to the database
+	describe("#save") do
+    it("adds a client to the array of saved clients") do
+      test_client = Client.new({:client_name => "Eve", :stylist_id => 1})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
 end
