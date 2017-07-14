39616dff6b3f349b24354c0836635d0306768176
Victor Allen <vamuigua@gmail.com>
2017-07-14 14:55:55 +0300

    Added attribute_accessors and initialize method

---
+++ lib/client.rb
@@ -0,0 +1,10 @@
+class Client
+
+	#attribute_accessor
+	attribute_accessor(:client_name,:stylist_id)
+
+	define_method(:initialize) do |attributes|
+		@client_name = attributes.fetch(:client_name)
+		@stylist_id = attributes.fetch(:stylist_id)
+	end
+end
