cef9679589f257c5e70034d549f110728fc8397d
Victor Allen <vamuigua@gmail.com>
2017-07-14 13:12:58 +0300

    Added spec to test and return stylist's name and id Addded spec to test and save a new stylist Added spec to make two object stylists created with the same name be the sam/equal to each other

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,5 +8,38 @@ describe(Stylist) do
 		it("The list of Stylists is empty at first") do
 			expect(Stylist.all()).to(eq([]))
 		end
-	end	
+	end
+
+	describe("#stylist_name") do
+      it("tells you the stylist's name") do
+        test_stylist = Stylist.new({:stylist_name => "Jane", :id => 1})
+        expect(test_stylist.stylist_name()).to(eq("Jane"))
+      end
+    end
+
+    describe("#id") do
+      it("sets the stylist's ID when you save it") do
+        test_stylist = Stylist.new({:stylist_name => "Jane", :id => 1})
+        test_stylist.save()
+        expect(test_stylist.id()).to(be_an_instance_of(Fixnum))
+      end
+    end
+
+	#spec to save a stylist and return the new stylist
+	describe("#save") do
+      it("lets you save stylists to the database") do
+        stylist = Stylist.new({:stylist_name => "Jane", :id => 1})
+        stylist.save()
+        expect(Stylist.all()).to(eq([stylist]))
+      end
+    end	
+
+    #spec to make stylist same if it has the same name
+    describe("#==") do
+      it("is the same stylist if it has the same name") do
+        stylist1 = Stylist.new({:stylist_name => "Jane", :id => 1})
+        stylist2 = Stylist.new({:stylist_name => "Jane", :id => 1})
+        expect(stylist1).to(eq(stylist2))
+      end
+    end
 end
