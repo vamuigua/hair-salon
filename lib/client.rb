aa41834fbd7e30e1795dd52f468561deefc7de6e
Victor Allen <vamuigua@gmail.com>
2017-07-14 15:06:40 +0300

    Commented out attribute_accessor

--- lib/client.rb
+++ lib/client.rb
@@ -1,7 +1,7 @@
 class Client
 
 	#attribute_accessor
-	attribute_accessor(:client_name,:stylist_id)
+	# attribute_accessor(:client_name,:stylist_id)
 
 	define_method(:initialize) do |attributes|
 		@client_name = attributes.fetch(:client_name)
