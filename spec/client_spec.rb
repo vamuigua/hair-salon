4ed0cc9b0b9473847f0cd5fa15e81f7ee0b9cf62
Victor Allen <vamuigua@gmail.com>
2017-07-14 16:43:04 +0300

    Added spec to make two objects ewith same properties be equal/same

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,16 +7,25 @@ describe(Client) do
 	#spec to return all the clients
 	describe(".all") do
 		it("returns empty clients from database") do
-        expect(Client.all()).to(eq([]))
+			expect(Client.all()).to(eq([]))
     	end
 	end
 
 	#spec to save a client to the database
 	describe("#save") do
-    it("adds a client to the array of saved clients") do
-      test_client = Client.new({:client_name => "Eve", :stylist_id => 1})
-      test_client.save()
-      expect(Client.all()).to(eq([test_client]))
+    	it("adds a client to the array of saved clients") do
+      		test_client = Client.new({:client_name => "Eve", :stylist_id => 1})
+      		test_client.save()
+      		expect(Client.all()).to(eq([test_client]))
+    	end
+  	end
+
+  	#to make two client objects with the same properties are treated the same in our specs
+    describe("#==") do
+      it("is the same client if it has the same name and stylist_id") do
+        test_client1 = Client.new({:client_name => "Joy", :stylist_id => 1})
+        test_client2 = Client.new({:client_name => "Joy", :stylist_id => 1})
+        expect(test_client1).to(eq(test_client2))
+      end
     end
-  end
 end
