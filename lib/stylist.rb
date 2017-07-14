058be5b21939de64151b4af713861d0981ad0d7d
Victor Allen <vamuigua@gmail.com>
2017-07-14 11:34:23 +0300

    Defined attribute accessor and defined initialize method

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,2 +1,11 @@
 class Stylist
+
+	#attribute_accessor to read and write
+	attr_accessor(:id,:stylist_name)
+
+	#define initialize method
+	define_method(:initialize) do |attributes|
+		@stylist_name = attributes.fetch(:stylist_name)
+		@id = attributes.fetch(:id)
+	end
 end
