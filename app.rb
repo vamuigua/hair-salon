245283aded1a726512ef49008bdaf29d4dcf7c9d
Victor Allen <vamuigua@gmail.com>
2017-07-16 16:39:37 +0300

    Modified app routes

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ end
 #post request to post the stylists
 post("/stylists") do
   stylist_name = params.fetch("stylist_name")
-  new_stylist = Stylist.new({:stylist_name => stylist_name})
+  new_stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
   new_stylist.save()
+  @stylists = Stylist.all()
   erb(:success)
 end
 
@@ -30,4 +31,29 @@ end
 get("/stylists") do
   @stylists = Stylist.all()
   erb(:stylists)
+end
+
+#post request to post client_name and id for a new Stylist created
+post("/clients") do
+  client_name = params.fetch("client_name")
+  client_id = params.fetch("id").to_i()
+  @client = Client.new({:client_name => client_name, :id => client_id})
+  @client.save()
+  erb(:stylist)
+end
+
+#get request to view specific stylist
+get("/stylists/:id") do
+  id = params.fetch("id").to_i()
+  @stylist = Stylist.find(id)
+  erb(:stylist)
+end
+
+#post stylists according to the id
+post("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i)
+  client_name = params.fetch("client_name")
+  new_client = Client.new({:client_name=>client_name})
+  @stylist.add_member(new_client)
+  erb(:stylist)
 end
